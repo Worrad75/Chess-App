deb6ee3b1e6e5fbecc7ec5f7d8aa93ef414fd904
github username <github email address>
2021-01-19 22:02:44 -0500

    successfully integrated drop and drag for the piece images

--- src/COMPONENTS/Piece.jsx
+++ src/COMPONENTS/Piece.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import useDrag from 'react-dnd'
+import {useDrag, DragPreviewImage} from 'react-dnd'
 
 
 export default function Piece({ piece: {type, color} }) {
-
+    const [{ isDragging} ,drag, preview] = useDrag({
+        item: { type: 'piece', id: `${type}_${color}` },
+        collect: (monitor) => {
+            return {isDragging: monitor.isDragging()}
+        }
+    })
     const pieceImg = require(`../assets/${type}_${color}.png`)
     // a black bishop would be "b_b.png" 
 
     return (
-    <div className="piece-container">
-        {}<img src={pieceImg} className="piece" />
-    {type}_{color}.png
-    </div>)
+    <>
+    {/* <DragPreviewImage connect={preview} src={pieceImg}/> */}
+                {type}_{color}
+        <div className="piece-container" ref={drag} style={{opacity: isDragging ? 0 : 1}}>
+            {}<img src={pieceImg} className="piece" />
+        </div>
+    </>)
 }
